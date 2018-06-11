ea26a557a987bb79fac270f351b696d4651c110d
geordidearns <geordidearns@gmail.com>
2018-06-11 12:54:41 +0200

    Add commitmsg support

---
+++ src/scripts/commitmsg.js
@@ -0,0 +1,15 @@
+const execa = require('execa');
+const ora = require('ora');
+
+const commitmsg = async () => {
+  const spinner = ora('zen commitmsg').start();
+  try {
+    await execa.shell('commitlint -e $GIT_PARAMS --config=${__dirname}/../configs/commitlint.config.js');
+  } catch (e) {
+    spinner.fail(`zen commitmsg failed with ${e}`);
+    return;
+  }
+  spinner.succeed(`zen commitmsg finished in ${process.uptime().toFixed(2)}s`);
+};
+
+exports.commitmsg = commitmsg;
