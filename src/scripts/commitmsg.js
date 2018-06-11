ac9ec1a72591c8080168b3b785cc1347b35a3be0
geordidearns <geordidearns@gmail.com>
2018-06-11 14:39:54 +0200

    Investigate commitmsg errors

--- src/scripts/commitmsg.js
+++ src/scripts/commitmsg.js
@@ -2,14 +2,19 @@ const execa = require('execa');
 const ora = require('ora');
 
 const commitmsg = async () => {
-  const spinner = ora('zen commitmsg').start();
+  // const spinner = ora('zen commitmsg').start();
   try {
-    await execa.shell(`commitlint -e $GIT_PARAMS --config=${__dirname}/../configs/commitlint.config.js`);
+    const { stdout, stderr } = await execa.shell(`commitlint -e $GIT_PARAMS --config=${__dirname}/../configs/commitlint.config.js`);
+    console.log('\n\n\nstdout=====')
+    console.log(stdout)
+    console.log('\n\n\nstderr=====')
+    console.log(stderr)
   } catch (e) {
-    spinner.fail(`zen commitmsg failed with ${e}`);
+    // spinner.fail(`zen commitmsg failed with ${e}`);
+    console.log(e);
     return;
   }
-  spinner.succeed(`zen commitmsg finished in ${process.uptime().toFixed(2)}s`);
+  // spinner.succeed(`zen commitmsg finished in ${process.uptime().toFixed(2)}s`);
 };
 
 exports.commitmsg = commitmsg;
